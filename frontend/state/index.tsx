b6639404165b6cadcdada23e528dfb70a69ae230
Vahid Turab AKBAY <66867524+VTAkbay@users.noreply.github.com>
2022-01-31 22:53:28 +0300

    feat: imported frontend structure (#1)
    * feat: imported frontend structure
    
    * refactor: rm unnecessary code
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

---
+++ frontend/state/index.tsx
@@ -0,0 +1,20 @@
+import { eth } from "state/eth"; // Eth state provider
+import type { ReactElement } from "react"; // Types
+
+/**
+ * State providing wrapper
+ * @param {ReactElement | ReactElement[]} children to inject
+ * @returns {ReactElement} wrapper
+ */
+export default function StateProvider({
+  children,
+}: {
+  children: ReactElement | ReactElement[];
+}): ReactElement {
+  return (
+    // Wrap in sub-providers
+    <eth.Provider>
+      <></>
+    </eth.Provider>
+  );
+}
