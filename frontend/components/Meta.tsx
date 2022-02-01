9614aa2099918bb67db5bddc2fdea12a07cd31d5
Cruz Molina <cruz.adam.molina@gmail.com>
2022-01-31 16:30:32 -0800

    style: refactor title, description, impish logo, & Home (#4)

--- frontend/components/Meta.tsx
+++ frontend/components/Meta.tsx
@@ -2,7 +2,7 @@ import Head from "next/head"; // HTML Head
 
 // Setup project details
 const url: string = process.env.NEXT_PUBLIC_URL ?? "https://token.com/";
-const tokenName: string = process.env.NEXT_PUBLIC_TOKEN_NAME ?? "Token Name";
+const title: string = process.env.NEXT_PUBLIC_TITLE ?? "Impish";
 const description: string =
   process.env.NEXT_PUBLIC_DESCRIPTION ?? "Some description";
 
@@ -22,21 +22,21 @@ export default function Meta() {
       />
 
       {/* Primary Meta */}
-      <title>{tokenName}</title>
-      <meta name="title" content={tokenName} />
+      <title>{title}</title>
+      <meta name="title" content={title} />
       <meta name="description" content={description} />
 
       {/* Open Graph + Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
-      <meta property="og:title" content={tokenName} />
+      <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={`${url}meta.png`} />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content={tokenName} />
+      <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={`${url}meta.png`} />
 
