14fa31de25d57d8fbd1e3c367c3cf418021070ad
TopETH <supernathanliu@gmail.com>
2022-02-01 13:47:50 -0500

    Prisma Integration (#5)
    * vendor: add prisma node module
    
    * feat: add DB_URL in .env.sample
    
    * feat: integrate prisma for login
    
    * build: update .env.sample
    
    * doc: update frontend/README
    
    * build: update package-lock
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

--- frontend/pages/api/login.js
+++ frontend/pages/api/login.js
@@ -1,10 +1,26 @@
 import { Magic } from "@magic-sdk/admin";
 import { setLoginSession } from "../../lib/auth";
+import prisma from "../../lib/prisma";
 
 export default async function login(req, res) {
   const magic = new Magic(process.env.MAGIC_SECRET_KEY);
   const didToken = req.headers.authorization.substr(7);
   const metadata = await magic.users.getMetadataByToken(didToken);
   await setLoginSession(res, metadata);
+  try {
+    const dbInfo = {
+      wallet: metadata.publicAddress,
+      email: metadata.email,
+    }
+    const result = await prisma.user.upsert({
+      where: {
+        email: metadata.email
+      },
+      create: dbInfo,
+      update: dbInfo,
+    });
+  } catch(e) {
+    console.error(e);
+  }
   res.send({ done: true });
 }
