15b2c6c115974ad1f58d23c16455188d719597a5
TopETH <supernathanliu@gmail.com>
2022-02-10 18:14:27 -0500

    fix: update avatar/banner behavior in profile page (#21)
    * fix: update avatar/banner behavior in profile page
    
    * fix: update cursor on photo view
    
    * fix: darken backdrop
    
    * style: misc + styling
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,12 +1,13 @@
 export const shortenAddress = (address: string) => {
-  if(!address)
-    return "";
-  if(address.startsWith("0x")) {
-    if(address.length < 12)
-      return address;
+  if (!address) return "";
+  if (address.startsWith("0x")) {
+    if (address.length < 12) return address;
     return address.slice(0, 6) + "..." + address.slice(-4);
   }
-  if(address.length < 10)
-    return address;
+  if (address.length < 10) return address;
   return address.slice(0, 4) + "..." + address.slice(-4);
-}
+};
+
+export const stopPropagation = (e: React.MouseEvent<HTMLElement>) => {
+  e.stopPropagation();
+};
