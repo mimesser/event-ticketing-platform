dec783a27cf9fc04b33caae8b62f95ec6d6b751c
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2022-02-17 19:31:14 -0800

    fix: login bug

--- frontend/lib/magicAdmin.ts
+++ frontend/lib/magicAdmin.ts
@@ -1,6 +1,3 @@
 import { Magic } from "@magic-sdk/admin";
-import { isBrowser } from "lib/utils";
 
-export const magic = isBrowser
-  ? new Magic(process.env.MAGIC_SECRET_KEY ?? "")
-  : undefined;
+export const magic = new Magic(process.env.MAGIC_SECRET_KEY ?? "");
