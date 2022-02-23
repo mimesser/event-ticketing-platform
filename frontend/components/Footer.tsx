dfb5b403f0feeca633229d63459fe8a320c69a05
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2022-02-23 15:52:09 -0800

    style: formatting

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,4 +1,4 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { useUser } from "lib/hooks";
 import { useRouter } from "next/router";
 import styles from "styles/components/Footer.module.scss"; // Component styles
@@ -7,15 +7,17 @@ export default function Footer() {
   const user = useUser({});
   const router = useRouter();
 
-  return !user ? <div className={styles.footer}>
-    <div className={styles.title}>Connect on Impish</div>
-    <Button
-      color="primary"
-      size="large"
-      variant="contained"
-      onClick={() => router.push("/")}
-    >
-      Log in / Sign up
-    </Button>
-  </div> : null;
+  return !user ? (
+    <div className={styles.footer}>
+      <div className={styles.title}>Connect on Impish</div>
+      <Button
+        color="primary"
+        size="large"
+        variant="contained"
+        onClick={() => router.push("/")}
+      >
+        Log in / Sign up
+      </Button>
+    </div>
+  ) : null;
 }
