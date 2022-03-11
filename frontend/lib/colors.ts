9d4ea071aabf7fbbd4f83b8b6803bbb6ac3e7468
TopETH <supernathanliu@gmail.com>
2022-03-10 21:10:39 -0500

    Topeth/header update darkmode (#89)
    * style: DARK MODE
    
    * style: background color of menu items
    
    * fix: remove 'following' button on invalid profile page
    
    * style: hover color
    
    * style: border and hover
    
    * fix: use custom color for selected drawer menu
    
    * style: misc
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

--- frontend/lib/colors.ts
+++ frontend/lib/colors.ts
@@ -1,14 +1,18 @@
 const Colors: any = {
   dark: {
     header_bg: "#242526",
-    drawer_bg: "rgb(21, 24, 28)",
+    drawer_bg: "#18191a",
     primary: "rgb(217, 217, 217)",
     secondary: "rgb(110, 118, 125)",
     event_drawer_bg: "#242526",
     input_border: "rgb(110, 118, 125)",
-    event_border: "1px solid #3E4042",
-    event_divider: "#3E4042",
+    border: "1px solid #3E4042",
+    divider: "#3E4042",
     selected_event_menu: "rgba(255,255,255,0.1)",
+    account_menu_shadow:
+      "0 12px 28px 0 rgba(0, 0, 0, 0.2),0 2px 4px 0 rgba(0, 0, 0, 0.1),inset 0 0 0 1px rgba(255, 255, 255, 0.05)",
+    hover: "rgba(255,255,255,0.1)",
+    selected_drawer_menu: "rgba(45, 136, 255, 0.2)",
   },
   light: {
     header_bg: "#FFFFFF",
