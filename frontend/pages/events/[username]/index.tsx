b16ba2eaa230876fe5efddf5cb3d38767bcba777
tobi akinrinade <71441407+thobbyAk@users.noreply.github.com>
2022-03-11 22:48:44 +0100

    feat(events): added authorization guard on users events (#93)

--- frontend/pages/events/[username]/index.tsx
+++ frontend/pages/events/[username]/index.tsx
@@ -1,4 +1,5 @@
 import Layout from "components/Layout";
+import { getLoginSession } from "lib/auth";
 
 function UserEvents() {
   return (
@@ -6,6 +7,22 @@ function UserEvents() {
       <></>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const session = await getLoginSession(context.req);
+  if (session) {
+    return {
+      props: {},
+    };
+  } else {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/events`,
+      },
+    };
+  }
 }
 
 export default UserEvents;
