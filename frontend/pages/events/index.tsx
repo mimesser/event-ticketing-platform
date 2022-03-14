26607cdea3a5c4046184cbd4a7a95e5619d243de
tobi akinrinade <71441407+thobbyAk@users.noreply.github.com>
2022-03-14 19:02:32 +0100

    feat(events): added empty state for events (#92)
    * feat(events): added empty state for events
    
    * fix(events): fixed reeponsiveness for empty state

--- frontend/pages/events/index.tsx
+++ frontend/pages/events/index.tsx
@@ -1,9 +1,34 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
 import Layout from "components/Layout";
+import Colors from "lib/colors";
+import { useTheme } from "next-themes";
 
 function Events() {
+  const { resolvedTheme } = useTheme();
   return (
     <Layout>
-      <></>
+      <>
+        <Box sx={{ p: 4 }}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography
+              sx={{
+                color: Colors[resolvedTheme]?.secondary,
+              }}
+              variant="body1"
+            >
+              No events found.
+            </Typography>
+          </Grid>
+        </Box>
+      </>
     </Layout>
   );
 }
