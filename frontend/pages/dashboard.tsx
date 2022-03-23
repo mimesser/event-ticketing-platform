e6db4890ee45f214ffc318694666bb2cf212acb0
TopETH <supernathanliu@gmail.com>
2022-03-22 21:34:05 -0400

    feat: refactor signup flow (#128)
    * feat: refactor signup flow
    
    * refactor: rm deprecated onboarding prop
    
    * fix: layout render
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,9 +1,6 @@
-import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CloseIcon from "@mui/icons-material/Close";
-import Fade from "@mui/material/Fade";
 import IconButton from "@mui/material/IconButton";
 import Modal from "@mui/material/Modal";
 import Stack from "@mui/material/Stack";
@@ -11,14 +8,12 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import Typography from "@mui/material/Typography";
 import Colors from "lib/colors";
 import { useUser } from "lib/hooks";
-import { magic } from "lib/magic";
 import { moonPaySrc } from "lib/moon-pay";
 import Layout from "components/Layout";
-import { ethers } from "ethers";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "styles/pages/Dashboard.module.scss";
 import { Tooltip } from "@mui/material";
 import { useTheme } from "next-themes";
@@ -31,13 +26,8 @@ function Dashboard() {
     redirectTo: firstTimeUser ? false : "/dashboard",
     redirectIfFound: true,
   });
-  const [signupFlow, setSignupFlow] = useState(firstTimeUser ? true : false);
-  const [welcomeModal, setWelcomeModal] = useState(true); // Welcome modal
   const [moonpayModalForBanner, setMoonpayModalForBanner] = useState(false); // buy crypto on moonpay modal for banner
-  const [moonPayModal, setMoonPayModal] = useState(false); // buy crypto on moonpay modal
-  const [twitterModal, setTwitterModal] = useState(false); // Find frens on Twitter modal
   const [twitterModalForBanner, setTwitterModalForBanner] = useState(false); // Find frens on Twitter modal for banner
-  const [twitterButton, setTwitterButton] = useState(false);
 
   const [twitterBanner, setTwitterBanner] = useState(
     user?.twitterUsername === null ? true : false
@@ -47,78 +37,6 @@ function Dashboard() {
       ? true
       : false
   );
-
-  // Update user nativeAssetBalance
-  const updateBalance = useCallback(async () => {
-    const provider = new ethers.providers.Web3Provider(
-      magic?.rpcProvider as any
-    );
-
-    const balance = await provider.getBalance(user.walletAddress);
-
-    if (balance) {
-      try {
-        await fetch("/api/update-balance", {
-          method: "POST",
-          body: JSON.stringify({
-            balance: balance.toString(),
-          }),
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }, [user?.walletAddress]);
-
-  useEffect(() => {
-    if (user) {
-      updateBalance();
-    }
-  }, [signupFlow, user, updateBalance]);
-
-  const modalClose = () => {
-    // Create user signup notifications
-    fetch("/api/signup-notifications", {
-      method: "POST",
-    });
-
-    setSignupFlow(false);
-    setWelcomeModal(false);
-    setMoonPayModal(false);
-  };
-
-  // Buy crypto modal to find frens on Twitter modal
-  const continueToTwitter = () => {
-    setMoonPayModal(false);
-    setTwitterModal(true);
-  };
-
-  // MoonPay modal to to buy crypto
-  const continueToMoonPayModal = () => {
-    setWelcomeModal(false);
-    setMoonPayModal(true);
-    getUsersBalance();
-  };
-
-  //check users balance after moonplay load and show twitter button upon change in account balance
-  const getUsersBalance = async () => {
-    const provider = new ethers.providers.Web3Provider(
-      magic?.rpcProvider as any
-    );
-
-    const signer = provider.getSigner();
-    const userAddress = await signer.getAddress();
-    const balance = await provider.getBalance(userAddress);
-    let timer = setInterval(async () => {
-      const currentUserAddress = await signer.getAddress();
-      const currentUserBalance = await provider.getBalance(currentUserAddress);
-      if (currentUserBalance.gt(balance)) {
-        updateBalance();
-        setTwitterButton(true);
-        clearInterval(timer);
-      }
-    }, 250);
-  };
 
   const modalStyle = {
     position: "absolute",
@@ -247,7 +165,7 @@ function Dashboard() {
           </div>
         </Box>
       </Modal>
-      <Layout onboarding={signupFlow}>
+      <Layout>
         <Stack
           direction="row"
           mt={4}
@@ -363,131 +281,6 @@ function Dashboard() {
             </Tooltip>
           )}
         </Stack>
-        {user && signupFlow && (
-          <>
-            <Modal
-              BackdropComponent={Backdrop}
-              BackdropProps={{
-                timeout: 500,
-              }}
-              closeAfterTransition
-              onClose={modalClose}
-              open={signupFlow}
-              aria-labelledby="transition-modal-title"
-              aria-describedby="transition-modal-description"
-            >
-              <Fade in={signupFlow}>
-                <Box sx={modalStyle}>
-                  <div className={styles.modal_box}>
-                    <IconButton
-                      aria-label="close"
-                      onClick={modalClose}
-                      className={styles.close_button}
-                      sx={{
-                        ":hover": {
-                          backgroundColor: Colors[resolvedTheme].hover,
-                        },
-                      }}
-                    >
-                      <CloseIcon
-                        sx={{
-                          color: Colors[resolvedTheme].primary,
-                        }}
-                      />
-                    </IconButton>
-                    {/* Welcome modal */}
-                    {welcomeModal && (
-                      <div className={styles.modal_body}>
-                        <Typography id={styles.h5} variant="h5">
-                          Welcome to Impish
-                        </Typography>
-                        <Typography variant="h6">
-                          {"Let's"} get started!
-                        </Typography>
-                        <Box>
-                          <Button
-                            id={styles.continueButtons}
-                            onClick={continueToMoonPayModal}
-                            type="submit"
-                            size="large"
-                            variant="contained"
-                            endIcon={<ArrowRightIcon />}
-                          >
-                            Continue
-                          </Button>
-                        </Box>
-                      </div>
-                    )}
-                    {/* MoonPay modal */}
-                    {moonPayModal && (
-                      <div
-                        className={styles.modal_body}
-                        style={{ height: "60vh", margin: "12px" }}
-                      >
-                        <iframe
-                          allow="accelerometer; autoplay; camera; gyroscope; payment"
-                          frameBorder="0"
-                          height="100%"
-                          id="moonPayFrame"
-                          src={moonPaySrc(user.walletAddress, user.email)}
-                          width="100%"
-                        >
-                          <p>Your browser does not support iframes.</p>
-                        </iframe>
-                        {twitterButton && (
-                          <Box>
-                            <Button
-                              id={styles.continueButtons}
-                              onClick={continueToTwitter}
-                              type="submit"
-                              size="large"
-                              variant="contained"
-                              endIcon={<ArrowRightIcon />}
-                            >
-                              Continue
-                            </Button>
-                          </Box>
-                        )}
-                      </div>
-                    )}
-                    {/* Twitter modal */}
-                    {twitterModal && (
-                      <div className={styles.modal_body}>
-                        <Typography id={styles.h5} variant="h5">
-                          Find frens you follow on Twitter
-                        </Typography>
-                        <Typography id={styles.body1} variant="body1">
-                          To get the most of your Web3 adventure, connect with
-                          frens on Twitter.
-                        </Typography>
-                        <Box className={styles.linkSocialButtons}>
-                          <Button
-                            onClick={() =>
-                              signIn("twitter", { callbackUrl: "/twitter" })
-                            }
-                            id={styles.twtButton}
-                            type="submit"
-                            size="large"
-                            variant="outlined"
-                            sx={{
-                              backgroundColor: "rgb(29, 161, 242)",
-                              ":hover": {
-                                backgroundColor: "rgb(26, 140, 216)",
-                              },
-                            }}
-                            startIcon={<TwitterIcon />}
-                          >
-                            Find frens I follow
-                          </Button>
-                        </Box>
-                      </div>
-                    )}
-                  </div>
-                </Box>
-              </Fade>
-            </Modal>
-          </>
-        )}
       </Layout>
     </>
   ) : null;
