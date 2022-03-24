4075026b39620942592a30fac9e0c1983acc5058
Vahid Turab AKBAY <66867524+VTAkbay@users.noreply.github.com>
2022-03-25 01:55:05 +0300

    VTAkbay/feat/link-twitter-redirect (#133)
    * feat: redirect to the page the user was originally at
    
    * misc: rm twitter module css file
    
    * refactor: rm twitter page
    
    * feat: add twitter modal css to layout
    
    * feat: update nextauth api to get Login Session for update twitterUsername
    
    * feat: made useLocalStorage hook
    
    * feat: move Twitter modal to Layout
    
    * feat: import prisma & assign function before export
    
    * misc: rm unnecessary dependencies
    
    * fix: findFrens useEffect dependencies null error
    
    * feat: save to localStorage open-twitter-modal
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

--- frontend/lib/hooks.ts
+++ frontend/lib/hooks.ts
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Router from "next/router";
 import { useUserInfo } from "./user-context";
+import { isBrowser } from "./utils";
 
 export function useUser({ redirectTo, redirectIfFound }: any = {}) {
   const { user, loading } = useUserInfo();
@@ -39,4 +40,43 @@ export async function fetchPublicUser(username: any) {
       ).json()
     ).user ?? null;
   return user;
+}
+
+export function useLocalStorage(key: string) {
+  const [state, setState] = useState<string | null>(
+    useMemo(() => (isBrowser ? localStorage.getItem(key) : null), [key])
+  );
+
+  const setValue = useMemo(
+    () => (value: string | null) => {
+      if (!isBrowser) {
+        throw new Error("Local storage is available only in browser");
+      }
+
+      if (value == null) {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, value);
+      }
+
+      setState(value);
+    },
+    [key, setState]
+  );
+
+  useEffect(() => {
+    if (!isBrowser) {
+      return;
+    }
+
+    setState(localStorage.getItem(key));
+
+    const listener = () => {
+      setValue(localStorage.getItem(key));
+    };
+    window.addEventListener("storage", listener);
+    return () => window.removeEventListener("storage", listener);
+  }, [key, setValue]);
+
+  return [state, setValue] as const;
 }
