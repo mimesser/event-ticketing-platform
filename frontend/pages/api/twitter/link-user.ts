586120dfc5f3f9a02a0653fa1c1ce8dfc220f975
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2022-03-30 14:03:34 -0700

    refactor: add auth check to image upload api

--- frontend/pages/api/twitter/link-user.ts
+++ frontend/pages/api/twitter/link-user.ts
@@ -1,5 +1,5 @@
-import { getLoginSession } from "../../../lib/auth";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getLoginSession } from "lib/auth";
 import prisma from "lib/prisma";
 import { User } from "@prisma/client";
 
