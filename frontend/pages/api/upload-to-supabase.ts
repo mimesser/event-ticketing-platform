97a6bcc168c0e3df00e8f70ba0864e7664e6008f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2022-03-30 13:38:53 -0700

    fix: don't send back supabase key

--- frontend/pages/api/upload-to-supabase.ts
+++ frontend/pages/api/upload-to-supabase.ts
@@ -12,32 +12,32 @@ const uploadMiddleware = upload.single("file");
 apiRoute.use(uploadMiddleware);
 
 apiRoute.post(async (req: any, res: NextApiResponse) => {
-    const filename = req.query.filename;
-    const BUCKET_NAME: string = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_BUCKET_NAME as string;
-    const storage = supabase.storage.from(BUCKET_NAME);
-    const { error } = await storage
-        .upload(filename, req.file.buffer, {
-            cacheControl: "3600",
-            upsert: false,
-        });
+  const filename = req.query.filename;
+  const BUCKET_NAME: string = process.env
+    .NEXT_PUBLIC_SUPABASE_STORAGE_BUCKET_NAME as string;
+  const storage = supabase.storage.from(BUCKET_NAME);
+  const { error } = await storage.upload(filename, req.file.buffer, {
+    cacheControl: "3600",
+    upsert: false,
+  });
 
-    if (error) {
-        console.error(error);
-        res.status(500).json(error);
-        return;
-    }
+  if (error) {
+    console.error(error);
+    res.status(500).json(error);
+    return;
+  }
 
-    const { publicURL } = storage.getPublicUrl(filename);
+  const { publicURL } = storage.getPublicUrl(filename);
 
-    res.status(200).json({
-        url: publicURL + "?token=" + process.env.NEXT_PUBLIC_SUPABASE_KEY,
-    });
+  res.status(200).json({
+    url: publicURL,
+  });
 });
 
 export default apiRoute;
 
 export const config = {
-    api: {
-        bodyParser: false,
-    },
+  api: {
+    bodyParser: false,
+  },
 };
