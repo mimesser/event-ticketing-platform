79d51ccd1a0b4a9f34d21895151ed54cb4a7b0e4
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2022-03-30 14:03:34 -0700

    refactor: add auth check to image upload api

--- frontend/pages/api/upload-to-supabase.ts
+++ frontend/pages/api/upload-to-supabase.ts
@@ -1,4 +1,5 @@
 import { NextApiResponse } from "next";
+import { getLoginSession } from "lib/auth";
 import { supabase } from "lib/supabase";
 import multer from "multer";
 import nextConnect from "next-connect";
@@ -12,7 +13,22 @@ const uploadMiddleware = upload.single("file");
 apiRoute.use(uploadMiddleware);
 
 apiRoute.post(async (req: any, res: NextApiResponse) => {
+  const session = await getLoginSession(req);
+
+  if (!session) {
+    res.status(400).json({ error: "Missing session" });
+    return;
+  }
+
   const filename = req.query.filename;
+
+  if (!filename.includes(session.publicAddress)) {
+    res
+      .status(400)
+      .json({ error: "Not authorized to upload on behalf of user" });
+    return;
+  }
+
   const BUCKET_NAME: string = process.env
     .NEXT_PUBLIC_SUPABASE_STORAGE_BUCKET_NAME as string;
   const storage = supabase.storage.from(BUCKET_NAME);
