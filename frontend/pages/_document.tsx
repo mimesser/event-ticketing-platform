7da9644633897442fd1b42dd3fd21c94ccadeb97
Vahid Turab AKBAY <66867524+VTAkbay@users.noreply.github.com>
2022-04-07 00:48:45 +0300

    VTAkbay/install-cypress (#170)
    * feat: install Cypress
    
    * fix: example email default
    
    * refactor: import ServerSyleSheets from @mui/styles
    
    * feat: example.json to user.json
    
    * feat: git ignore Cypress user.json
    
    * feat: add token
    
    * misc: npm run cypress to cy
    
    * misc: rm token from example.json & update email to magic link test
    
    * feat: rm setCookie & login with email
    
    * feat: enable testMode Magic if not production
    
    * feat: add and use mock test user metadata if not production
    
    * feat: made npm run e2e script
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>
    Co-authored-by: Cruz Molina <cruz.adam.molina@gmail.com>

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -6,6 +6,8 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import { ServerStyleSheets } from "@mui/styles";
+import React from "react";
 
 // Custom document class
 export default class MyDocument extends Document {
@@ -29,3 +31,27 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
