570a505c111bcd67da93311842fa272ef50f7dd9
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2022-04-07 11:13:24 -0700

    refactor: simplify logout logic

--- frontend/pages/api/logout.ts
+++ frontend/pages/api/logout.ts
@@ -1,4 +1,3 @@
-import { QueryBuilder } from "@mui/icons-material";
 import { getLoginSession } from "lib/auth";
 import { removeTokenCookie } from "lib/auth-cookie";
 import { magic } from "lib/magicAdmin";
@@ -10,14 +9,15 @@ export default async function logout(
 ) {
   try {
     const session = await getLoginSession(req);
+
     if (session) {
       await magic?.users.logoutByIssuer(session.issuer);
       removeTokenCookie(res);
     }
+
+    res.status(200).json({ status: "User logged out" });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error });
   }
-  const query = req.query;
-  res.writeHead(302, { Location: query.route });
-  res.end();
 }
