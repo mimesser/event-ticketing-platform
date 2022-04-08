a9da0f269b421a11e20cd9ca5c436ec57480ed16
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2022-04-07 17:40:59 -0700

    refactor: updated anon-security.sql

--- frontend/migrations/anon-security.sql
+++ frontend/migrations/anon-security.sql
@@ -1,11 +1,35 @@
 -- protect notifications table from unlimited access with the anon key --
 -- enable RLS
 alter table notifications enable row level security;
--- allow anon users to read notifications
-create policy "anon-read-notifications" on public.notifications for select using (role() = 'anon'::text);
 
 -- grant limited column access to anon users
--- remove all previleges(`users` table) from anon users
-remove all on users from anon;
+-- remove all privileges(`notifications` table) from anon users
+revoke all on notifications from anon;
+-- grant read privileges for column 'id'
+grant select(id) on notifications to anon;
+
+-- protect events table from unlimited access with the anon key --
+-- enable RLS
+alter table events enable row level security;
+
+-- grant limited column access to anon users
+-- remove all privileges(`events` table) from anon users
+revoke all on events from anon;
+-- grant read privileges for column 'id'
+grant select(id) on events to anon;
+
+-- protect Follows table from unlimited access with the anon key --
+-- enable RLS
+alter table "Follows" enable row level security;
+-- allow anon users to read follows
+create policy "anon-read-follows" on public."Follows" for select using (role() = 'anon'::text);
+
+-- protect users table from unlimited access with the anon key --
+-- enable RLS
+alter table users enable row level security;
+
+-- grant limited column access to anon users
+-- remove all privileges(`users` table) from anon users
+revoke all on users from anon;
 -- grant read privileges for column 'id', 'username'
 grant select(id, username) on users to anon;
