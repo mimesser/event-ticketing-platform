39ecfe78de46e9e18b39e1b6727acf7a3e09784b
TopETH <supernathanliu@gmail.com>
2022-04-12 18:40:31 -0400

    Event Cover Photo (#192)
    * feat: cover photo in event create
    
    * fix: resolve conflict in events/create.tsx
    
    * fix: remove illustration from cover setting
    
    * fix: reformat drawer component
    
    * fix: change tab with spaces in [username]/index.tsx
    
    * fix: remove placeholder images
    
    * style: misc
    
    * style: misc
    
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

--- frontend/lib/event-context.tsx
+++ frontend/lib/event-context.tsx
@@ -2,78 +2,83 @@ import moment from "moment";
 import { ReactElement, createContext, useContext, useState } from "react";
 
 const EventContext = createContext<{
-  eventName?: string;
-  eventLocation?: any;
-  timezone?: any;
-  eventDescription?: string;
-  startDate?: string;
-  endDate?: string;
-  privacy?: string;
-  invitable?: boolean;
+	eventName?: string;
+	eventLocation?: any;
+	timezone?: any;
+	eventDescription?: string;
+	startDate?: string;
+	endDate?: string;
+	privacy?: string;
+	invitable?: boolean;
+	cover?: any;
 
-  setEventName?: any;
-  setEventLocation?: any;
-  setTimezone?: any;
-  setEventDescription?: any;
-  setStartDateAndTime?: any;
-  setEndDateAndTime?: any;
-  setEventPrivacy?: any;
-  setEventInvitable?: any;
+	setEventName?: any;
+	setEventLocation?: any;
+	setTimezone?: any;
+	setEventDescription?: any;
+	setStartDateAndTime?: any;
+	setEndDateAndTime?: any;
+	setEventPrivacy?: any;
+	setEventInvitable?: any;
+	setCover?: any;
 }>({});
 
 export const useNewEvent = () => useContext(EventContext);
 
 export default function EventProvider({
-  children,
+	children,
 }: {
-  children: ReactElement | ReactElement[];
+	children: ReactElement | ReactElement[];
 }) {
-  const [eventName, setEventName] = useState("");
-  const [eventLocation, setEventLocation] = useState({});
-  const [timezone, setTimezone] = useState({});
-  const [eventDescription, setEventDescription] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [privacy, setEventPrivacy] = useState("");
-  const [invitable, setEventInvitable] = useState(true);
+	const [eventName, setEventName] = useState("");
+	const [eventLocation, setEventLocation] = useState({});
+	const [timezone, setTimezone] = useState({});
+	const [eventDescription, setEventDescription] = useState("");
+	const [startDate, setStartDate] = useState("");
+	const [endDate, setEndDate] = useState("");
+	const [privacy, setEventPrivacy] = useState("");
+	const [invitable, setEventInvitable] = useState(true);
+	const [cover, setCover] = useState({});
 
-  const setStartDateAndTime = (date: string, time: string) => {
-    const formatted = moment(date).format("YYYY-MM-DD ") + time;
-    setStartDate(formatted);
-  };
+	const setStartDateAndTime = (date: string, time: string) => {
+		const formatted = moment(date).format("YYYY-MM-DD ") + time;
+		setStartDate(formatted);
+	};
 
-  const setEndDateAndTime = (show: boolean, date: string, time: string) => {
-    if (show) {
-      const formatted = moment(date).format("YYYY-MM-DD ") + time;
-      setEndDate(formatted);
-    } else {
-      setEndDate("");
-    }
-  };
+	const setEndDateAndTime = (show: boolean, date: string, time: string) => {
+		if (show) {
+			const formatted = moment(date).format("YYYY-MM-DD ") + time;
+			setEndDate(formatted);
+		} else {
+			setEndDate("");
+		}
+	};
 
-  return (
-    <EventContext.Provider
-      value={{
-        eventName,
-        eventLocation,
-        timezone,
-        eventDescription,
-        startDate,
-        endDate,
-        privacy,
-        invitable,
+	return (
+		<EventContext.Provider
+			value={{
+				eventName,
+				eventLocation,
+				timezone,
+				eventDescription,
+				startDate,
+				endDate,
+				privacy,
+				invitable,
+				cover,
 
-        setEventName,
-        setEventLocation,
-        setTimezone,
-        setEventDescription,
-        setStartDateAndTime,
-        setEndDateAndTime,
-        setEventPrivacy,
-        setEventInvitable,
-      }}
-    >
-      {children}
-    </EventContext.Provider>
-  );
+				setEventName,
+				setEventLocation,
+				setTimezone,
+				setEventDescription,
+				setStartDateAndTime,
+				setEndDateAndTime,
+				setEventPrivacy,
+				setEventInvitable,
+				setCover,
+			}}
+		>
+			{children}
+		</EventContext.Provider>
+	);
 }
