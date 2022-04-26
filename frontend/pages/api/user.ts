b74dab2b28f5286b7a03fd35574f599c16cc79f7
Vahid Turab AKBAY <66867524+VTAkbay@users.noreply.github.com>
2022-04-26 20:38:16 +0300

    feat: assign moonpayurl on user api (#229)

--- frontend/pages/api/user.ts
+++ frontend/pages/api/user.ts
@@ -1,6 +1,11 @@
 import { getLoginSession } from "../../lib/auth";
 import prisma from "lib/prisma";
 import { NextApiResponse, NextApiRequest } from "next";
+import { isProduction } from "lib/utils";
+import createHmac from "create-hmac";
+
+const TEST_KEY = process.env.NEXT_PUBLIC_MOONPAY_TEST_KEY;
+const SECRET_KEY = process.env.NEXT_PUBLIC_MOONPAY_SECRET_KEY;
 
 export default async function user(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -34,6 +39,25 @@ export default async function user(req: NextApiRequest, res: NextApiResponse) {
         following: true,
       },
     });
+
+    const originalUrl = `https://buy-sandbox.moonpay.com?apiKey=${TEST_KEY}&currencyCode=${
+      // todo: should be MATIC_POLYGON in production, but would need to turn off Test Mode on MoonPay
+      isProduction ? "ETH" : "ETH"
+    }&walletAddress=${session.walletAddress}&email=${encodeURIComponent(
+      session.email
+    )}`;
+
+    const signature = createHmac("sha256", `${SECRET_KEY}`)
+      .update(new URL(originalUrl).search)
+      .digest("base64");
+
+    const urlWithSignature = `${originalUrl}&signature=${encodeURIComponent(
+      signature
+    )}`;
+
+    if (user?.id) {
+      Object.assign(user, { moonpayUrl: urlWithSignature });
+    }
 
     res.status(200).json({ user: user || null });
   } catch (e) {
