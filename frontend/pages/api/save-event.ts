a985df11c40d080a53ba43bb20d98c4d7f4e4ac6
Vahid Turab AKBAY <66867524+VTAkbay@users.noreply.github.com>
2022-04-27 20:27:58 +0300

    VTAkbay/create-event-redirect (#234)
    * feat: redirect to event after created
    
    * feat: add redirect check to create event test

--- frontend/pages/api/save-event.ts
+++ frontend/pages/api/save-event.ts
@@ -90,7 +90,7 @@ apiRoute.post(async (req: any, res: NextApiResponse) => {
     await prisma.coHosts.createMany({
       data: coHostInfo,
     });
-    res.status(200).json({ status: "ok" });
+    res.status(200).json({ status: "ok", eventId: eventId });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error });
