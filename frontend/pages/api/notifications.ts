af87198d4bb8a9ba0361262bb3a0ddbfde2370e8
Vahid Turab AKBAY <66867524+VTAkbay@users.noreply.github.com>
2022-04-28 23:35:44 +0300

    feat: api error handling (#239)

--- frontend/pages/api/notifications.ts
+++ frontend/pages/api/notifications.ts
@@ -7,52 +7,74 @@ export default async function notifications(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getLoginSession(req);
-  const { notifications }: any = JSON.parse(req.body);
+  try {
+    if (req.method !== "DELETE") {
+      res.status(400).json({ error: "Wrong method! Only DELETE" });
+      return;
+    }
 
-  if (!session || !notifications) {
-    res.status(400).json({ error: "Missing session or notifications object" });
-    return;
-  }
+    const session = await getLoginSession(req);
 
-  if (
-    !Array.isArray(notifications) ||
-    !notifications.every((id: any) => typeof id === "number")
-  ) {
-    res
-      .status(400)
-      .json({ error: "Wrong type for notifications, only number objects" });
-    return;
-  }
+    if (!session) {
+      res.status(400).json({ error: "Missing session" });
+      return;
+    }
 
-  if (req.method === "DELETE") {
-    const userId: any = await prisma.user.findUnique({
-      where: { email: session.email },
-      select: { id: true },
-    });
+    if (!req.body) {
+      res.status(400).json({ error: "Missing request body" });
+      return;
+    }
 
-    try {
-      await prisma.notification.updateMany({
-        where: { id: { in: notifications }, userId: userId.id },
+    const { notifications } = JSON.parse(req.body);
 
-        data: {
-          isRead: true,
-        },
+    if (!notifications) {
+      res
+        .status(400)
+        .json({ error: "Missing notifications object in request body" });
+      return;
+    }
+
+    if (
+      !Array.isArray(notifications) ||
+      !notifications.every((id: any) => typeof id === "number")
+    ) {
+      res
+        .status(400)
+        .json({ error: "Wrong type for notifications, only number objects" });
+      return;
+    }
+
+    if (req.method === "DELETE") {
+      const userId: any = await prisma.user.findUnique({
+        where: { email: session.email },
+        select: { id: true },
       });
 
-      if (isTest) {
+      try {
         await prisma.notification.updateMany({
           where: { id: { in: notifications }, userId: userId.id },
 
           data: {
-            isRead: false,
+            isRead: true,
           },
         });
-      }
 
-      res.status(200).json({ status: "Mark notificatons as read success" });
-    } catch (error) {
-      res.status(500).json({ error });
+        if (isTest) {
+          await prisma.notification.updateMany({
+            where: { id: { in: notifications }, userId: userId.id },
+
+            data: {
+              isRead: false,
+            },
+          });
+        }
+
+        res.status(200).json({ status: "Mark notificatons as read success" });
+      } catch (error) {
+        res.status(500).json({ error });
+      }
     }
+  } catch (e) {
+    res.status(500).json({ e });
   }
 }
--- frontend/pages/api/twitter/link-user.ts
+++ frontend/pages/api/twitter/link-user.ts
@@ -7,43 +7,56 @@ export default async function linkUser(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getLoginSession(req);
+  try {
+    if (req.method !== "POST" && req.method !== "DELETE") {
+      res.status(400).json({ error: "Wrong method! Only POST or DELETE" });
+      return;
+    }
+    const session = await getLoginSession(req);
 
-  if (!session) {
-    res.status(400).json({ error: "Missing session" });
-    return;
-  }
-
-  if (req.method === "POST") {
-    const { twitterUsername }: User = JSON.parse(req.body);
-
-    if (!twitterUsername) {
-      res.status(400).json({ error: "Missing twitterUsername" });
+    if (!session) {
+      res.status(400).json({ error: "Missing session" });
       return;
     }
 
-    try {
-      await prisma.user.update({
-        where: { email: session.email },
-        data: { twitterUsername: twitterUsername },
-      });
+    if (req.method === "POST") {
+      if (!req.body) {
+        res.status(400).json({ error: "Missing request body" });
+        return;
+      }
 
-      res.status(200).json({ status: "Link Twitter success" });
-    } catch (e) {
-      res.status(500).json({ e });
+      const { twitterUsername }: User = JSON.parse(req.body);
+
+      if (!twitterUsername) {
+        res.status(400).json({ error: "Missing twitterUsername" });
+        return;
+      }
+
+      try {
+        await prisma.user.update({
+          where: { email: session.email },
+          data: { twitterUsername: twitterUsername },
+        });
+
+        res.status(200).json({ status: "Link Twitter success" });
+      } catch (e) {
+        res.status(500).json({ e });
+      }
     }
-  }
 
-  if (req.method === "DELETE") {
-    try {
-      await prisma.user.update({
-        where: { email: session.email },
-        data: { twitterUsername: null },
-      });
+    if (req.method === "DELETE") {
+      try {
+        await prisma.user.update({
+          where: { email: session.email },
+          data: { twitterUsername: null },
+        });
 
-      res.status(200).json({ status: "Unlink Twitter success" });
-    } catch (e) {
-      res.status(500).json({ e });
+        res.status(200).json({ status: "Unlink Twitter success" });
+      } catch (e) {
+        res.status(500).json({ e });
+      }
     }
+  } catch (e) {
+    res.status(500).json({ e });
   }
 }
