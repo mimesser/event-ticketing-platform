bd28bf0273a6ece2c665d628520641acc7f8a0cd
cuteOlaf <53915161+cuteOlaf@users.noreply.github.com>
2022-04-28 10:02:31 -0700

    feat: filter going events (#227)
    * feat: implement event view page
    
    * feat: filter going events
    
    * fix: remove mockup data from event-by-id api
    
    * fix: update event view page styling
    
    * fix: remove /events/<username>, add /events/<eventID> routes
    
    * fix: event filter routing
    
    * remove: /events/[username]
    
    * fix: event filter list selection behavior
    
    * fix: going events styling
    
    * fix: event filter routing
    
    * fix: merge events/[eventid] and events/[filter] into one route
    
    * feat: going events styling
    
    * fix: change icon when cover photo is not provided
    time string for Sunday
    remove mock data for filtered events
    
    * fix: remove customized string for next week
    
    * fix: add time comparation to event filter.
    fix: handle case with no going events
    
    * fix: time string formatting for today's events
    
    * fix: styling for "No events Found", default cover photo
    
    * style: misc
    
    Co-authored-by: TopETH <supernathanliu@gmail.com>
    Co-authored-by: CruzMolina <7537712+CruzMolina@users.noreply.github.com>

---
+++ frontend/components/GoingEvents.tsx
@@ -0,0 +1,282 @@
+import React from "react";
+import {
+  Button,
+  CircularProgress,
+  Divider,
+  MenuList,
+  MenuItem,
+  Typography,
+  Box,
+  Grid,
+} from "@mui/material";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Colors from "lib/colors";
+import CalendarViewMonthIcon from "@mui/icons-material/CalendarViewMonth";
+import { EventDetails } from "lib/types";
+import { useEventsFilter } from "lib/hooks";
+import { groupEventsByMonth } from "lib/utils";
+import { useRouter } from "next/router";
+
+export default function GoingEvents() {
+  const { resolvedTheme } = useTheme();
+  const { loading, events } = useEventsFilter("going");
+  const groupedEvents = groupEventsByMonth(events);
+  const router = useRouter();
+  const viewEvent = (eventId: number) => {
+    router.push("/events/" + eventId);
+  };
+
+  const onClickDetails = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <>
+      {loading || !events ? (
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress size={120} />
+        </div>
+      ) : events.length === 0 ? (
+        <Box sx={{ p: 4 }}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography
+              sx={{
+                color: Colors[resolvedTheme]?.secondary,
+              }}
+              variant="body1"
+            >
+              No events found.
+            </Typography>
+          </Grid>
+        </Box>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            marginLeft: "15%",
+            maxWidth: "75%",
+            maxHeight: "calc(100% - 100px)",
+          }}
+        >
+          <div>
+            <Typography
+              gutterBottom
+              variant="h6"
+              sx={{
+                color: Colors[resolvedTheme]?.primary,
+                textAlign: "left",
+                fontWeight: 900,
+                marginTop: "16px",
+                marginBottom: "0px",
+              }}
+            >
+              Going
+            </Typography>
+            <Typography
+              sx={{
+                color: Colors[resolvedTheme]?.secondary,
+              }}
+              variant="body2"
+            >
+              {events.length === 0
+                ? "No events"
+                : events.length === 1
+                ? "1 Event"
+                : events.length + " Events"}
+            </Typography>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              overflow: "auto",
+              width: "100%",
+            }}
+          >
+            {groupedEvents.map(({ monthName, events, dayStr }, index) => (
+              <MenuList
+                sx={{
+                  width: "100%",
+                  borderRadius: "10px",
+                  marginTop: "20px",
+                  backgroundColor: Colors[resolvedTheme].eventItem_bg,
+                  color: Colors[resolvedTheme].primary,
+                  boxShadow:
+                    "0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%)",
+                }}
+                key={index}
+              >
+                <MenuItem
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    border: "none",
+                    ":hover": { background: "transparent" },
+                  }}
+                >
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    sx={{
+                      color: Colors[resolvedTheme]?.primary,
+                      backgroundColor: Colors[resolvedTheme].eventItem_bg,
+                      textAlign: "left",
+                      fontWeight: 900,
+                      alignSelf: "start",
+                    }}
+                  >
+                    {monthName}
+                  </Typography>
+                  {monthName === "Today" ? (
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      sx={{
+                        color: Colors[resolvedTheme]?.secondary,
+                        backgroundColor: Colors[resolvedTheme].eventItem_bg,
+                        textAlign: "left",
+                        fontWeight: 600,
+                        fontSize: "15px",
+                        alignSelf: "start",
+                      }}
+                    >
+                      {dayStr}
+                    </Typography>
+                  ) : (
+                    <></>
+                  )}
+                </MenuItem>
+                {events.map((event: EventDetails, index: number) => (
+                  <div key={index}>
+                    {index ? (
+                      <Divider
+                        sx={{
+                          margin: "0px 10px",
+                          padding: "0px",
+                          borderColor: Colors[resolvedTheme].divider,
+                        }}
+                      />
+                    ) : (
+                      <div></div>
+                    )}
+                    <MenuItem
+                      key={index}
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        padding: "10px",
+                        border: "none",
+                        ":hover": {
+                          backgroundColor:
+                            Colors[resolvedTheme].eventItem_hover,
+                          borderRadius: "5px",
+                        },
+                      }}
+                      onClick={() => viewEvent(event.id)}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          width: 200,
+                          height: 150,
+                          borderRadius: 15,
+                          overflow: "hidden",
+                          background: Colors[resolvedTheme].no_cover_photo_bg,
+                        }}
+                      >
+                        {event.coverPhoto.url ? (
+                          <Image
+                            src={event.coverPhoto.url}
+                            key={index}
+                            alt="Cover photo"
+                            width={200}
+                            height={150}
+                          />
+                        ) : (
+                          <CalendarViewMonthIcon
+                            sx={{
+                              width: "48px",
+                              height: "48px",
+                              color: Colors[resolvedTheme]?.primary,
+                            }}
+                          />
+                        )}
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          marginLeft: 15,
+                          flex: 1,
+                          gap: "2px",
+                        }}
+                      >
+                        <span style={{ fontWeight: 600 }}>
+                          {event.startTime}
+                        </span>
+                        <span style={{ fontWeight: "bold", fontSize: 20 }}>
+                          {event.title}
+                        </span>
+
+                        {event.location.hasLocation ? (
+                          <span style={{ fontWeight: 500 }}>
+                            {event.location.name}
+                          </span>
+                        ) : (
+                          <br />
+                        )}
+
+                        <span
+                          style={{ fontSize: 14, fontWeight: 600 }}
+                        >{`${event.going} Going`}</span>
+                      </div>
+                      <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <Button
+                          onClick={(e) => onClickDetails(e)}
+                          sx={{
+                            background:
+                              Colors[resolvedTheme].eventDetailsBtn_bg,
+                            borderRadius: "5px",
+                            padding: "0px",
+                            fontSize: 16,
+                            color: Colors[resolvedTheme].primary,
+                            fontWeight: 900,
+                            width: "36px",
+                            height: "32px",
+                            minWidth: "32px",
+                          }}
+                        >
+                          ...
+                        </Button>
+                      </div>
+                    </MenuItem>
+                  </div>
+                ))}
+              </MenuList>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ frontend/pages/api/get-events.ts
@@ -0,0 +1,64 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { EventDetails, LocationInfo } from "lib/types";
+import prisma from "lib/prisma";
+import { getLoginSession } from "../../lib/auth";
+import moment from "moment";
+
+export default async function getEvents(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { filter } = JSON.parse(req.body);
+
+  // TODO: filter events
+  // filter: going, host, invites, past, ...
+  try {
+    const session = await getLoginSession(req);
+    if (!session) {
+      res.status(500).json("should log in");
+      return;
+    }
+    const user = await prisma.user.findUnique({
+      where: { email: session.email },
+      select: { id: true },
+    });
+    const today = new Date(moment().format("YYYY-MM-DD") + " 00:00:00");
+    const events = await prisma.event.findMany({
+      where: {
+        hostId: user?.id,
+        startTime: {
+          gte: today,
+        },
+      },
+      orderBy: {
+        startTime: "asc",
+      },
+    });
+
+    let eventDetails: EventDetails[] = [];
+
+    events.map((event) => {
+      const coverPhoto = JSON.parse(event.coverPhoto || "{}");
+      const location: LocationInfo = JSON.parse(event.location || "{}") || {};
+      const startTime = moment(event.startTime);
+      const now = moment();
+      if (startTime.dayOfYear() === now.dayOfYear()) {
+        if (event.endTime && moment(event.endTime).isAfter(now)) return;
+      }
+      eventDetails.push({
+        id: event.id,
+        title: event.title || "",
+        description: event.description || "",
+        startTime: event.startTime ? event.startTime.toUTCString() : "",
+        endTime: event.endTime ? event.endTime.toUTCString() : "",
+        location,
+        going: 1, // TODO:
+        coverPhoto,
+      });
+    });
+    res.status(200).json({ events: eventDetails });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+}
