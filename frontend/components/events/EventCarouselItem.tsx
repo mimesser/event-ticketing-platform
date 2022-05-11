9b8000b03680250daf73f156f259e70241f1c267
mimesser <michaelmesser9507@gmail.com>
2022-05-11 14:13:20 -0700

    feat: events carousel integration (#264)
    * feat: events carousel integration
    
    * misc: refactor

---
+++ frontend/components/events/EventCarouselItem.tsx
@@ -0,0 +1,205 @@
+import React from "react";
+import { Button, MenuItem, Tooltip, Typography } from "@mui/material";
+import CalendarViewMonthIcon from "@mui/icons-material/CalendarViewMonth";
+import { useTheme } from "next-themes";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import moment from "moment";
+import Colors from "lib/colors";
+import { EventDetails, EventDetailsOption } from "lib/types";
+import { formatTimeString } from "lib/utils";
+import { Box } from "@mui/system";
+
+export default function EventCarouselItem({
+  layout,
+  event,
+  options,
+}: {
+  layout: "vertical" | "horizontal";
+  event: EventDetails;
+  options: EventDetailsOption;
+}) {
+  const router = useRouter();
+  const { resolvedTheme } = useTheme();
+  const viewEvent = (eventId: number) => {
+    router.push("/events/" + eventId);
+  };
+
+  const isEventGoing = (event: EventDetails) => {
+    const startTime = moment(event.startTime);
+    const now = moment();
+
+    if (now.isBefore(startTime)) return true;
+    if (now.isSame(startTime, "day")) {
+      if (!event.endTime) return true;
+      return now.isBefore(moment(event.endTime));
+    }
+    return false;
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        position: "relative",
+        borderRadius: "15px",
+        backgroundColor: Colors[resolvedTheme].eventItem_bg,
+        color: Colors[resolvedTheme].primary,
+        marginLeft: "8px",
+        marginRight: "8px",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 0,
+          padding: "0px",
+          border: "none",
+          ":hover": {
+            borderRadius: "15px",
+            background: Colors[resolvedTheme].eventItem_hover,
+            "& > .div_cover_photo": {
+              background: "rgba(228, 230, 234,0.6)",
+            },
+          },
+          alignItems: "flex-start",
+          width: "100%",
+        }}
+        onClick={() => viewEvent(event.id)}
+      >
+        {/* Cover Photo */}
+        <Box
+          component="div"
+          className="div_cover_photo"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "15px",
+            borderBottomLeftRadius: "0px",
+            borderBottomRightRadius: "0px",
+            overflow: "hidden",
+            background: Colors[resolvedTheme].no_cover_photo_bg,
+            width: "100%",
+            aspectRatio: "16/9",
+          }}
+        >
+          {event.coverPhoto?.url ? (
+            <img
+              src={event.coverPhoto?.url}
+              alt="Cover photo"
+              style={{
+                width: "100%",
+                aspectRatio: "16/9",
+              }}
+            />
+          ) : (
+            <CalendarViewMonthIcon
+              sx={{
+                width: "48px",
+                height: "48px",
+                color: Colors[resolvedTheme]?.primary,
+              }}
+            />
+          )}
+        </Box>
+        {/* Event Details */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1,
+            justifyContent: "center",
+            marginTop: "16px",
+            paddingBottom: "50px",
+            overflowWrap: "break-word",
+            width: "100%",
+            gap: "8px",
+          }}
+        >
+          <span style={{ fontWeight: 600, marginLeft: "15px" }}>
+            {formatTimeString(event.startTime)}
+          </span>
+          <Tooltip
+            title={
+              <React.Fragment>
+                <Typography sx={{ marginLeft: "15px" }}>
+                  <b>{event.title}</b>
+                </Typography>
+              </React.Fragment>
+            }
+          >
+            <Typography
+              sx={{
+                fontWeight: "bold",
+                fontSize: 20,
+                marginLeft: "15px",
+                width: "fit-content",
+                ":hover": {
+                  textDecoration: "underline",
+                },
+              }}
+              variant="body1"
+            >
+              {event.title}
+            </Typography>
+          </Tooltip>
+
+          <span style={{ fontWeight: 500, marginLeft: "15px" }}>
+            {event.location?.hasLocation ? event.location.name : ""}
+          </span>
+
+          <span
+            style={{ fontSize: 14, fontWeight: 600, marginLeft: "15px" }}
+          >{`${event.count} ${isEventGoing(event) ? "Going" : "Went"}`}</span>
+        </div>
+      </Box>
+      {options.showDetailsMenu && (
+        <Button
+          onClick={(e) => {
+            options.onDetails(event);
+            options.showMenu(e.currentTarget);
+          }}
+          sx={
+            layout === "horizontal"
+              ? {
+                  position: "absolute",
+                  alignItems: "baseline",
+                  borderRadius: "5px",
+                  padding: "0px",
+                  fontSize: 16,
+                  background: Colors[resolvedTheme].eventDetailsBtn_bg,
+                  color: Colors[resolvedTheme].primary,
+                  fontWeight: 900,
+                  right: "16px",
+                  top: "32px",
+                  width: "48px",
+                  height: "36px",
+                  minWidth: "16px",
+                  ":hover": {
+                    background: Colors[resolvedTheme].eventDetailsBtn_hover,
+                  },
+                }
+              : {
+                  position: "absolute",
+                  top: "8px",
+                  right: "8px",
+                  borderRadius: "50%",
+                  minWidth: "32px",
+                  height: "32px",
+                  width: "32px",
+                  background: Colors[resolvedTheme].eventDetailsBtn_bg2,
+                  color: "white",
+                  ":hover": {
+                    background: Colors[resolvedTheme].eventDetailsBtn_hover2,
+                  },
+                }
+          }
+        >
+          ...
+        </Button>
+      )}
+    </div>
+  );
+}
